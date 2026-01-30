0cd60f7696b65984e1bb880ca412517e0fc32601
sakinahussain <sakinarasheed007@gmail.com>
2026-01-30 15:15:47 -0500

    Started working on the 1st part of the project.

---
+++ src/uno/players/BeginnerPlayer.java
@@ -0,0 +1,107 @@
+package uno.players;
+/*
+Title       : BeginnerPlayer.java
+Description : A very simple Uno player that always playe the first legal card.
+Author      : Sakina Hussain
+Date        : 01/29/2026
+Version     : 1.0
+Java Version: Java 24
+ */
+
+// This import allows us to use all the Uno classes provided for the project
+// Examples: Card, Player, WildCard, ColoredCard, etc.
+import uno.*;
+
+// This import allows use to use the List type, which stores the cards in our hand
+import java.util.List;
+public class BeginnerPlayer {
+    /*
+    * This method is REQUIRED by the Player interface.
+    * It is called when we need to choose which card to play.
+    *
+    * top - the card currently on top of the discard pile
+    * hand - a list of all the cards in our hand
+    * color - the current color (important after a Wild card)
+    *
+    * We return:
+    * - the index of the card we want to play
+    * - OR -1 if we cannot play any card
+     */
+    public int makeChoice(Card top, List<Card> hand, ColoredCard.Color color ) {
+
+        // We go through the hand one card at a time
+        for (int i = 0; i < hand.size(); i ++) {
+
+            // Get the current card form our hand
+            Card currentCard = hand.get(i);
+
+            // If the card is a Wild card, it is always legal to play
+            if (currentCard instanceof WildCard) {
+                // Play this card by returning its position
+                return i;
+            }
+
+            // If the card has a color (meaning it is not a Wild)
+            if  (currentCard instanceof ColoredCard) {
+
+                // Convert the card into a ColoredCard so we can check its color
+                ColoredCard coloredCard = (ColoredCard) currentCard;
+
+                // If the card's colore matches the current color, it is legal
+                if (coloredCard.getColor() == color) {
+                    return i;
+                }
+
+                // If the card type matches the top card type (number/action),
+                // then it is also legal to play
+                if (currentCard.getClass() == top.getClass()) {
+                    return i;
+                }
+            }
+        }
+        // If we reach here, we found no legal card
+        // Returning -1 means we must draw a card
+        return -1;
+    }
+    /*
+    *This method is REQUIRED by the Player interface.
+    * It is called after we play a Wild or Wild Draw Four card
+    *
+    * For simplicity, we always choose RED.
+    * This is legal and easy for a beginner.
+     */
+    public ColoredCard.Color pickColor() {
+        return ColoredCard.Color.RED;
+    }
+    /*
+    * This methods below are required by the Player interface.
+    * but the assignment allow them to empty.
+    * We include them so the program will compile correctly.
+     */
+
+    /*
+    * Called when the game starts.
+    * We are not using this information, so we leave it empty/
+     */
+    public void startGame (int player, int index, Card start) {
+        // No setup needed for this simple player
+    }
+
+    /*
+    * Called when any player plays a card.
+    * We are not tracking other players.
+     */
+    public void playerPlays (int index, Card card) {
+        // No action needed
+    }
+
+    /*
+    * Called when a player chooses a color.
+    * We are not tracking color choices.
+     */
+    public void playerPicks (ColoredCard.Color color) {
+        // No action needed
+    }
+
+    /*
+}
