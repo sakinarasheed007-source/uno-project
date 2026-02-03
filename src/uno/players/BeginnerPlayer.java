922f60d6be0a6aea4d819e2259f5cf4f1af5e425
sakinahussain <sakinarasheed007@gmail.com>
2026-02-03 13:29:14 -0500

    Add required class files

--- src/uno/players/BeginnerPlayer.java
+++ src/uno/players/BeginnerPlayer.java
@@ -2,7 +2,7 @@ package uno.players;
 /*
 Title       : BeginnerPlayer.java
 Description : A very simple Uno player that always playe the first legal card.
-Author      : Sakina Hussain
+Author      : Sakina Hussain, Bethany Mixon
 Date        : 01/29/2026
 Version     : 1.0
 Java Version: Java 24
